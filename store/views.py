74045ba6c6141d8782a1094bbd2785140e31f7a9
Zihadul Haque Talukdar <zihadkuet@yahoo.com>
2021-12-20 18:50:25 +0600

    project

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Product
 from .serializers import ProductSerializer
