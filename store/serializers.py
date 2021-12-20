6985cb03910387bec32d8c0f6c856c4d467a4ce6
Zihadul Haque Talukdar <zihadkuet@yahoo.com>
2021-12-20 16:17:49 +0600

    ModelSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -8,17 +8,12 @@ class CollectionSerializer(serializers.Serializer):
     id=serializers.IntegerField()
     title=serializers.CharField(max_length=255)
 
-class ProductSerializer(serializers.Serializer):
-    id=serializers.IntegerField()
-    title=serializers.CharField(max_length=255)
-    price=serializers.DecimalField(source='unit_price',max_digits=6,decimal_places=2)
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Product
+        fields=['id','title','unit_price','price_with_tax','collection']
+
     price_with_tax=serializers.SerializerMethodField(method_name='calculate_tax')
-    # collection=serializers.StringRelatedField(
-    #     # queryset=Collection.objects.all()
-    # )
-    # collection=CollectionSerializer()
-    collection=serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),view_name='collection-detail'
-    )
+   
     def calculate_tax(self,product:Product):
         return product.unit_price*Decimal(1.11)
